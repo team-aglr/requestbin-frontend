7687890fc9730de43e8f3c893c988242db837c74
Albert Dorfman <albertdorfman@gmail.com>
2023-01-26 01:49:42 -0500

    alignment change

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,7 +4,7 @@ import BinPreview from "../components/BinPreview";
 function Home() {
   return (
     <div className="mx-auto max-w-2xl mt-12">
-      <div className="flex justify-between">
+      <div className="flex justify-between items-center">
         <h1 className="text-2xl font-bold">Your Bins</h1>
         <button className="bg-gradient-to-b from-primary-400 to-primary-500 text-white px-3 py-3 rounded-md hover:opacity-90">
           + Create a bin
